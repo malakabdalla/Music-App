d26ddff362bb76f152e3474cf8b0dbd4470847a1
malak <malookaabdalla@gmail.com>
2024-02-19 16:42:48 +0000

    css changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Intro from './Intro';
 import Song from './Song';
-// import CoverArt from './CoverArt';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Intro />
     <Song/>
+  
   </React.StrictMode>,
 )
