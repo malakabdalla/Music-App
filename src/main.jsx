64ef720cad3d1a74ca1cc2942cdc905764d19293
malak <malookaabdalla@gmail.com>
2024-02-19 14:50:18 +0000

    main fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Intro from './Intro';
+import Song from './Song';
+import CoverArt from './CoverArt';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Intro />
+    <CoverArt/>
+    <Song/>
   </React.StrictMode>,
 )
